0886147e2897c1ed1ef0c6d4a8c22c6fdd63e940
Johnny Cubides <jgcubidesc@gmail.com>
2023-05-04 11:30:17 -0500

    fix update example

--- gameMCUconnectUDP/love/main.lua
+++ gameMCUconnectUDP/love/main.lua
@@ -3,25 +3,20 @@ Udp = socket.udp()
 Udp:setsockname('*', 3000)
 Udp:settimeout(0)
 
-local greenX, greenY = 100, 100
 local redX, redY = 400, 100
 
 function love.draw()
-	love.graphics.setColor(0, 1, 0)
-	love.graphics.rectangle("fill", greenX, greenY, 50, 50)
-
 	love.graphics.setColor(1, 0, 0)
 	love.graphics.rectangle("fill", redX, redY, 50, 50)
 end
 
 function love.update()
-	greenX, greenY = love.mouse.getPosition()
 	local data, msg_or_ip, port_or_nil = Udp:receivefrom()
 	if data then
     print(data)
 		local p = Split(data, '-')
 		redX, redY = p[1], p[2]
-		Udp:sendto(tostring(greenX)..'-'..tostring(greenY), msg_or_ip, port_or_nil)
+		Udp:sendto(tostring(redX)..'-'..tostring(redY), msg_or_ip, port_or_nil)
 	end
 end
 
--- gameMCUconnectUDP/micropython-esp32/main.py
+++ gameMCUconnectUDP/micropython-esp32/main.py
@@ -1,24 +1,30 @@
 import usocket as socket
 
-# serverAddressPort = ("127.0.0.1", 3000) # local
-serverAddressPort = ("192.168.1.7", 3000) # red
+# Agregar ip y port de donde está el servidor UDP
+serverAddressPort = ("192.168.1.7", 3000)
+# Cantidad de bytes a recibir
 bufferSize  = 128
 
 # Create a UDP socket at client side
 sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# Función para enviar datos
 def sendUDP(data):
     sk.sendto(str.encode(data), serverAddressPort)
 
+# Función para recibir datos
 def receiveUDP():
     msg = sk.recvfrom(bufferSize)
     return "{}".format(msg[0])
  
+# Activar si el esp32 será una estación
 from wifiSTA import connectSTA as connect
+
+# Activar si el esp32 estará en modo de acceso AP
 #from wifiAP import apConfig as connect
 
 # poner acá el nombre de red ssid y password para conectarse
 connect("ssid", "password")
 
-# Enviar un dato por socket UPD
+# Función que podrá usar para enviar datos al servidor UDP
 sendUDP("350-350")
