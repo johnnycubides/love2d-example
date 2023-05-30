0a766d785afd3454bc69ee8705488100064f684d
Johnny Cubides <jgcubidesc@gmail.com>
2023-05-30 09:28:03 -0500

    update example

--- gameMCUconnectUDP/micropython-esp32/wifiAP.py
+++ gameMCUconnectUDP/micropython-esp32/wifiAP.py
@@ -2,7 +2,7 @@ def apConfig(ssid, pwd):
     import network
     ap_if = network.WLAN(network.AP_IF)
     ap_if.active(True)
-    ap_if.config(essid=ssidm, password=pwd)
+    ap_if.config(essid=ssid, password=pwd)
     while not ap_if.active():
         pass
     print('network config:', ap_if.ifconfig())
